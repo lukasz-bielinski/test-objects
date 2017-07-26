cc933e175076380a64a8491aa0f6c23cf7530d81
pulse <antynomia@gmail.com>
2017-07-26 13:38:46 +0200

    test

--- change.sh
+++ change.sh
@@ -3,3 +3,6 @@ grep -l -r "image: nginx" . | xargs sed -i  's|.*image: nginx.*|        image: n
 
 cd ../../../secrets/default
 grep -l -r "test" . | xargs sed -i 's|.*test.*|test3|g'
+
+#cd deployments/default/nginx
+#grep -l -r "replicas: " . | xargs sed -i 's|.*replicas\:.*|  replicas\: 20|g'
