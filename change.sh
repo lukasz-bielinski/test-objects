44ff56e41bf83923739fb282107d853568b7ec1a
pulse <antynomia@gmail.com>
2017-07-26 10:57:34 +0200

    test

--- change.sh
+++ change.sh
@@ -1,5 +1,5 @@
 cd deployments/default/nginx
-grep -l -r "image: nginx" . | xargs sed -i  's|.*image: nginx.*|        image: nginx:1.13-alpine|g'
+grep -l -r "image: nginx" . | xargs sed -i  's|.*image: nginx.*|        image: nginx:1.12-alpine|g'
 
 cd ../../../secrets/default
-grep -l -r "test" . | xargs sed -i 's|.*test.*|test1|g'
+grep -l -r "test" . | xargs sed -i 's|.*test.*|test2|g'
