eaf5e0568633414d2dfa2ccedad1835718adafe2
Lukasz Bielinski <l.bielinski@oberthur.com>
2017-07-27 11:00:43 +0200

    test

--- change.sh
+++ change.sh
@@ -1,8 +1,8 @@
 cd deployments/default/nginx
-grep -l -r "image: nginx" . | xargs sed -i  's|.*image: nginx.*|        image: nginx:1.12-alpine|g'
+grep -l -r "image: nginx" . | xargs sed -i  's|.*image: nginx.*|        image: nginx:1.13-alpine|g'
 
 cd ../../../secrets/default
-grep -l -r "test" . | xargs sed -i 's|.*test.*|test3|g'
+grep -l -r "test" . | xargs sed -i 's|.*test.*|test2|g'
 
 #cd deployments/default/nginx
 #grep -l -r "replicas: " . | xargs sed -i 's|.*replicas\:.*|  replicas\: 20|g'
